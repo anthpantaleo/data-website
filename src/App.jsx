457df7b193efde759728ca3f567ec2d80de6f421
anthpantaleo <anthpantaleo@gmail.com>
2022-12-08 16:44:32 -0500

    some hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import * as React from "react";
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 }
